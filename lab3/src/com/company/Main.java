51a45b12512208bece2fc79bef1913d9c271714f
markgardie <markgardie@users.noreply.github.com>
2021-12-09 18:27:15 +0200

    create findMax and findMin methods

---
+++ lab3/src/com/company/Main.java
@@ -0,0 +1,74 @@
+package com.company;
+
+import java.sql.Array;
+import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
+public class Main {
+    public static void main(String[] args) {
+        int[] array = {4, 10, 1, 5, 11, 6, 8, 3, 5, 5};
+        System.out.println(findMin(array).get());
+        System.out.println(findMax(array).get());
+        System.out.println(findElem(array, 5).get());
+    }
+
+    public static AtomicInteger findMin(int[] array) {
+        AtomicInteger atomicMin = new AtomicInteger();
+        atomicMin.set(array[0]);
+        IntStream.of(array).parallel().forEach( x -> {
+            int oldValue;
+            int newValue;
+            do {
+                oldValue = atomicMin.get();
+                if (x < oldValue) {
+                    newValue = x;
+                }
+                else {
+                    newValue = oldValue;
+                }
+            }while (!atomicMin.compareAndSet(oldValue, newValue));
+        });
+
+        return atomicMin;
+    }
+
+    public static AtomicInteger findMax(int[] array) {
+        AtomicInteger atomicMax = new AtomicInteger();
+        atomicMax.set(array[0]);
+        IntStream.of(array).parallel().forEach(x -> {
+            int oldValue;
+            int newValue;
+            do {
+                oldValue = atomicMax.get();
+                if (x > oldValue){
+                    newValue = x;
+                }
+                else {
+                    newValue = oldValue;
+                }
+
+            }while (!atomicMax.compareAndSet(oldValue, newValue));
+        });
+
+        return atomicMax;
+    }
+
+    public static AtomicInteger findElem(int[] array, int condition) {
+        AtomicInteger atomicCondition = new AtomicInteger();
+        atomicCondition.set(condition);
+        IntStream.of(array).parallel().forEach(x -> {
+            int oldValue;
+            int newValue;
+            do {
+                oldValue = atomicCondition.get();
+                if (x == condition) {
+                    oldValue++;
+                }
+                newValue = oldValue;
+
+            }while (!atomicCondition.compareAndSet(oldValue, newValue));
+
+        });
+        return atomicCondition;
+    }
+}
