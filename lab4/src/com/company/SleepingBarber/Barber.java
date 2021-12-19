b97109afacab489115dd90c872a4aff3551634ad
markgardie <markgardie@users.noreply.github.com>
2021-12-19 19:30:54 +0200

    implement Barber

--- lab4/src/com/company/SleepingBarber/Barber.java
+++ lab4/src/com/company/SleepingBarber/Barber.java
@@ -1,4 +1,106 @@
 package com.company.SleepingBarber;
 
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Barber {
+    private final int limit;
+    public int customers;
+
+    private final Semaphore customerReady;
+    private final Semaphore barberReady;
+
+    private final Semaphore customerDone;
+    private final Semaphore barberDone;
+
+    private final ReentrantLock mutex;
+
+
+    public Barber(int limit) {
+        this.limit = limit;
+        customers = 0;
+
+        customerReady = new Semaphore(0);
+        barberReady = new Semaphore(0);
+
+        customerDone = new Semaphore(0);
+        barberDone = new Semaphore(0);
+
+        mutex = new ReentrantLock();
+
+    }
+
+    public void startService() {
+
+        // wait for a customer to arrive
+        try {
+            customerReady.acquire();
+        } catch (InterruptedException e) {
+            System.out.println("Barber wait task is interrupted: " + e.getMessage());
+        }
+
+        // signal that barber is ready
+        barberReady.release();
+
+        // give a haircut
+        cutHair();
+
+
+        // wait for a customer to approve done
+        try {
+            customerDone.acquire();
+        } catch (InterruptedException e) {
+            System.out.println("Barber wait task is interrupted: " + e.getMessage());
+        }
+        // signal the customer that barber is done
+        barberDone.release();
+        System.out.println("Haircut is done");
+    }
+
+    public Void receiveNewCustomer() {
+
+        Customer customer = new Customer();
+
+        customer.enter();
+
+        mutex.lock();
+        if (customers == limit) {
+            mutex.unlock();
+            customer.leave();
+        }
+        customers += 1;
+        mutex.unlock();
+
+        customerReady.release();
+
+        // wait for the barber to be available
+        try {
+            barberReady.acquire();
+        } catch (InterruptedException e) {
+            System.out.println("Customer wait task is interrupted: " + e.getMessage());
+        }
+
+        // get the haircut
+        customer.getHairCut();
+
+        // signal that customer is done
+        customerDone.release();
+
+        // wait for barber to approve done
+        try {
+            barberDone.acquire();
+        } catch (InterruptedException e) {
+            System.out.println("Customer wait task is interrupted: " + e.getMessage());
+        }
+
+        mutex.lock();
+        customers -= 1;
+        mutex.unlock();
+
+        return null;
+    }
+
+    public void cutHair() {
+        System.out.println("Barber: Cutting Hair --- " + Thread.currentThread().getName());
+    }
 }
